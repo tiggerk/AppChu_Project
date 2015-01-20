092f962db6a3b8ecff0cd069801770011a0284b1
tiggerk <tiggerhyun@gmail.com>
2015-01-20 11:25:31 +0900

    style add..

--- beautyChuSpring_K/src/main/java/beautychu/domain/StylePhoto.java
+++ beautyChuSpring_K/src/main/java/beautychu/domain/StylePhoto.java
@@ -6,20 +6,21 @@ public class StylePhoto implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	
-	int stylePhotoNumber;
-	int styleNumber;
+	int stylePhotoNo;
+	int styleNo;
 	String url;
+	
 	public int getStylePhotoNumber() {
-		return stylePhotoNumber;
+		return stylePhotoNo;
 	}
-	public void setStylePhotoNumber(int stylePhotoNumber) {
-		this.stylePhotoNumber = stylePhotoNumber;
+	public void setStylePhotoNumber(int stylePhotoNo) {
+		this.stylePhotoNo = stylePhotoNo;
 	}
 	public int getStyleNumber() {
-		return styleNumber;
+		return styleNo;
 	}
-	public void setStyleNumber(int styleNumber) {
-		this.styleNumber = styleNumber;
+	public void setStyleNumber(int styleNo) {
+		this.styleNo = styleNo;
 	}
 	public String getUrl() {
 		return url;
@@ -29,8 +30,8 @@ public class StylePhoto implements Serializable {
 	}
 	@Override
 	public String toString() {
-		return "StylePhoto [stylePhotoNumber=" + stylePhotoNumber
-				+ ", styleNumber=" + styleNumber + ", url=" + url
+		return "StylePhoto [stylePhotoNo=" + stylePhotoNo
+				+ ", styleNo=" + styleNo + ", url=" + url
 				+ "]";
 	}
 	
