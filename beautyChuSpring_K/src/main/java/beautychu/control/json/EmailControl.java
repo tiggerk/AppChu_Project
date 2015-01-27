51e2aedefb08be4d4119a4570974b23a5f129d3d
tiggerk <tiggerhyun@gmail.com>
2015-01-27 18:40:46 +0900

    EmailControl, img추가, admin 수정

---
+++ beautyChuSpring_K/src/main/java/beautychu/control/json/EmailControl.java
@@ -0,0 +1,26 @@
+package beautychu.control.json;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller("json.StyleControl")
+public class EmailControl {
+
+	@Autowired
+	JavaMailSender mailSender;
+
+	@RequestMapping("/email/send")
+	public String send(String to, String subject, String content) {
+		SimpleMailMessage message = new SimpleMailMessage();
+		message.setFrom("아이디@gmail.com");
+		message.setTo(to);
+		message.setSubject(subject);
+		message.setText(content);
+		mailSender.send(message);
+		return "/email/sendResult.jsp";
+	}
+
+}
